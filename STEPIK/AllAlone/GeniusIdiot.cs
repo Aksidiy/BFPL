14fc9a58efe692ef2c7830e86f0c8d35bf88fa15
Aksyonov Dmitriy <dimon020397@gmail.com>
2026-08-02 21:42:43 +1200

    Обновил игнор

---
+++ STEPIK/AllAlone/GeniusIdiot.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BFPL.STEPIK.AllAlone
+{
+    public class GeniusIdiot
+    {
+        // Cписки вопросов и ответов
+        public string[] Questions { get; }
+        public int[] Answers { get; }
+        // Счетчик правильных ответов
+        private int correctAnswersCount = 0;
+
+        // Заполняем списки вопросов и ответов
+        public GeniusIdiot(string[] questions, int[] answers)
+        {
+            if (questions.Length != 5 || answers.Length != 5)
+            {
+                throw new ArgumentOutOfRangeException($"Количество вопросов и ответов должно быть равно 5. Переданные ");
+            }
+            Questions = questions;
+            Answers = answers;
+        }
+
+        // Цикл опроса
+        public void StartTestCircle()
+        {
+            // Обнуляем счётчик
+            correctAnswersCount = 0;
+
+            List<int> indexes = new List<int>() { 0, 1, 2, 3, 4 };
+            Shuffle(indexes); // Не LINQ, Правило: "пока курс не прошёл, не трогаю".
+            indexes.ForEach(item => Console.WriteLine(item));
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(Questions[indexes[i]]);           // а) Выводим вопрос
+
+                Console.Write("Ваш ответ: ");              // б) Запрашиваем ответ
+                int userAnswer = int.Parse(Console.ReadLine() ?? "0");
+
+                if (userAnswer == Answers[indexes[i]])              // в) Сравниваем
+                {
+                    correctAnswersCount++;                 // г) Увеличиваем счетчик
+                }
+            }
+        }
+
+        // Тасование Фишера–Йетса
+        public static void Shuffle<T>(List<T> list)
+        {
+            Random rng = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                // Меняем текущий элемент N со случайным индексом K
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        // Вывод результата
+        public void GetCorrectAnswersCount()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Количество правильных ответов: {correctAnswersCount}");
+        }
+
+        // Пример работы
+        public static void ExampleOfWork()
+        {
+            // 1. Создаем списки вопросов и ответов
+            string[] questions = new string[5];
+            questions[0] = "Сколько будет 2 плюс 2, умноженное на 2?";
+            questions[1] = "Бревно нужно распилить на 10 частей. Сколько надо сделать распилов?";
+            questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
+            questions[3] = "Укол делают каждые полчаса. Сколько нужно минут для трех уколов?";
+            questions[4] = "5 свечей горело, 2 потухли. Сколько свечей осталось?";
+
+            int[] answers = new int[5];
+            answers[0] = 6;
+            answers[1] = 9;
+            answers[2] = 25;
+            answers[3] = 60;
+            answers[4] = 2;
+
+            GeniusIdiot geniusIdiot = new GeniusIdiot(questions, answers);
+
+            // 3. Цикл опроса
+            geniusIdiot.StartTestCircle();
+            // 4. Вывод результата
+            geniusIdiot.GetCorrectAnswersCount();
+        }
+    }
+}
---
+++ STEPIK/STEPIK_MainMenu.cs
@@ -0,0 +1,132 @@
+﻿using BFPL.STEPIK.AllAlone;
+using BFPL.STEPIK.Basic_Plus_Sintax;
+using BFPL.STEPIK.Basic_Sintax;
+
+
+namespace BFPL.STEPIK
+{
+    public class MainMenu
+    {
+        //Вывод в консоль меню BFPL
+        public static void PrintBFPL_STEPIK_MainMenu()
+        {
+            Console.WriteLine
+                (
+                "\nBFPL STEPIC branch main menu:\n" +
+                "\tChapter 1: Basic Syntax.\n" +
+                "\t\tItem 1 - XesAndZeros\n" +
+                "\t\tItem 2 - PRGSW\n" +
+                "\tChapter 2: Basic Syntax.\n" +
+                "\t\tItem 1 - School Simulation\n" +
+                "\t\tItem 2 - Сommon Fractions Simulation\n" +
+                "\t\tItem 3 - Online Store Simulation\n" +
+                "\tChapter 3: All Alone.\n" +
+                "\t\tItem 1 - GeniusIdiot\n" +
+                "\tclear - Clear console\n" +
+                "\tmenu - Show STEPIC branch main menu\n" +
+                "\treturn - Return to BFPL main menu\n" +
+                "Write chapter and item separated by a space or use keywords:\n"
+                );
+        }
+
+        //Обработчик меню BFPL
+        public static void CallSTEPIKMainMenu()
+        {
+            Console.WriteLine(
+                "Welcome to BFPL - Big Fucking Practice Library!\n" +
+                "It`s STEPIC branch."
+                );
+            PrintBFPL_STEPIK_MainMenu();
+
+            while (true)
+            {
+                //Ловим ввод юзверя и на всякий в нижний регистр понижаем
+                //Формат ввода: [chapter number][space][item number] или [ключевое слово]
+                string shosenPath = Console.ReadLine().ToLower();
+
+                //Можно было бы сделать свитч, но он меня раздражает
+                switch (shosenPath)
+                {
+                    case "1 1":
+                        {
+                            Console.WriteLine($"You chose {shosenPath} path.\n");
+                            XesAndZeros.GoXesAndZeros();
+                            PrintBFPL_STEPIK_MainMenu();
+                            break;
+                        }
+
+                    case "1 2":
+                        {
+                            Console.WriteLine($"You chose {shosenPath} path.\n");
+                            string mathExpBeforePRGSW = $"( ( - 2 + 3 ) * ( 4 - 1 ) ) ^ ( 2 + ( 1 + 1 ) )";
+                            string mathExpAfterPRGSW = $"2 - 3 + 4 1 - * 2 1 1 + + ^";
+                            Console.WriteLine(mathExpBeforePRGSW);
+                            Console.WriteLine(PRGSW.polishReverseGripSteeringWheel(mathExpBeforePRGSW));
+                            Console.WriteLine(mathExpAfterPRGSW);
+                            Console.WriteLine(PRGSW.undoPolishReverseGripSteeringWheel(mathExpAfterPRGSW));
+                            Console.WriteLine(mathExpBeforePRGSW);
+                            Console.WriteLine(PRGSW.calculatePolishReverseGripSteeringWheel(mathExpBeforePRGSW, false));
+                            Console.WriteLine(mathExpAfterPRGSW);
+                            Console.WriteLine(PRGSW.calculatePolishReverseGripSteeringWheel(mathExpAfterPRGSW, true));
+                            PrintBFPL_STEPIK_MainMenu();
+                            break;
+                        }
+
+                    case "2 1":
+                        {
+                            Console.WriteLine($"You chose {shosenPath} path.\n");
+                            School.StartSchoolSimulation();
+                            break;
+                        }
+
+                    case "2 2":
+                        {
+                            Fraction first = new Fraction(3, 9);
+                            first.PrintFraction();
+                            Fraction second = new Fraction(63, 21);
+                            second.PrintFraction();
+                            int third = 5;
+
+
+                            (first + second).PrintFraction();
+                            (first - second).PrintFraction();
+                            (first * second).PrintFraction();
+                            (first / second).PrintFraction();
+
+                            (first + third).PrintFraction();
+                            (first - third).PrintFraction();
+                            (first * third).PrintFraction();
+                            (first / third).PrintFraction();
+
+                            (third + first).PrintFraction();
+                            (third - first).PrintFraction();
+                            (third * first).PrintFraction();
+                            (third / first).PrintFraction();
+
+                            Fraction last = new Fraction(3, 0);
+                            break;
+                        }
+                    case "3 1":
+                        Console.WriteLine($"You chose {shosenPath} path.\n");
+                        GeniusIdiot.ExampleOfWork();
+
+                        break;
+                    case "menu":
+                        PrintBFPL_STEPIK_MainMenu();
+                        break;
+                    case "clear":
+                        Console.Clear();
+                        PrintBFPL_STEPIK_MainMenu();
+                        break;
+                    case "return":
+                        //Go to BFPL main menu
+                        BFPL.BFPL_MainMenu.PrintMainMenu();
+                        return;
+                    default:
+                        Console.WriteLine($"Path: [{shosenPath}] do not exist.\n");
+                        break;
+                }
+            }
+        }
+    }
+}
