f8d37a47b08c7cf3836f6708222d16e834ca0dd3
Aksyonov Dmitriy <dimon020397@gmail.com>
2026-08-09 16:17:43 +1200

    Чутка обновил меню BFPL.Stepik.

--- STEPIK/STEPIK_MainMenu.cs
+++ STEPIK/STEPIK_MainMenu.cs
@@ -22,6 +22,7 @@ namespace BFPL.STEPIK
                 "\t\tItem 3 - Online Store Simulation\n" +
                 "\tChapter 3: All Alone.\n" +
                 "\t\tItem 1 - GeniusIdiot\n" +
+                "\t\tItem 2 - GeniusIdiot\n" +
                 "\tclear - Clear console\n" +
                 "\tmenu - Show STEPIC branch main menu\n" +
                 "\treturn - Return to BFPL main menu\n" +
@@ -44,7 +45,7 @@ namespace BFPL.STEPIK
                 //Формат ввода: [chapter number][space][item number] или [ключевое слово]
                 string shosenPath = Console.ReadLine().ToLower();
 
-                //Можно было бы сделать свитч, но он меня раздражает
+                //Вот теперь через свитч
                 switch (shosenPath)
                 {
                     case "1 1":
@@ -54,81 +55,112 @@ namespace BFPL.STEPIK
                             PrintBFPL_STEPIK_MainMenu();
                             break;
                         }
-
                     case "1 2":
                         {
                             Console.WriteLine($"You chose {shosenPath} path.\n");
-                            string mathExpBeforePRGSW = $"( ( - 2 + 3 ) * ( 4 - 1 ) ) ^ ( 2 + ( 1 + 1 ) )";
-                            string mathExpAfterPRGSW = $"2 - 3 + 4 1 - * 2 1 1 + + ^";
-                            Console.WriteLine(mathExpBeforePRGSW);
-                            Console.WriteLine(PRGSW.polishReverseGripSteeringWheel(mathExpBeforePRGSW));
-                            Console.WriteLine(mathExpAfterPRGSW);
-                            Console.WriteLine(PRGSW.undoPolishReverseGripSteeringWheel(mathExpAfterPRGSW));
-                            Console.WriteLine(mathExpBeforePRGSW);
-                            Console.WriteLine(PRGSW.calculatePolishReverseGripSteeringWheel(mathExpBeforePRGSW, false));
-                            Console.WriteLine(mathExpAfterPRGSW);
-                            Console.WriteLine(PRGSW.calculatePolishReverseGripSteeringWheel(mathExpAfterPRGSW, true));
-                            PrintBFPL_STEPIK_MainMenu();
+                            RunPRGSW();
                             break;
                         }
-
                     case "2 1":
                         {
                             Console.WriteLine($"You chose {shosenPath} path.\n");
                             School.StartSchoolSimulation();
                             break;
                         }
-
                     case "2 2":
                         {
-                            Fraction first = new Fraction(3, 9);
-                            first.PrintFraction();
-                            Fraction second = new Fraction(63, 21);
-                            second.PrintFraction();
-                            int third = 5;
-
-
-                            (first + second).PrintFraction();
-                            (first - second).PrintFraction();
-                            (first * second).PrintFraction();
-                            (first / second).PrintFraction();
-
-                            (first + third).PrintFraction();
-                            (first - third).PrintFraction();
-                            (first * third).PrintFraction();
-                            (first / third).PrintFraction();
-
-                            (third + first).PrintFraction();
-                            (third - first).PrintFraction();
-                            (third * first).PrintFraction();
-                            (third / first).PrintFraction();
-
-                            //Fraction last = new Fraction(3, 0);
-
-                            PrintBFPL_STEPIK_MainMenu();
+                            Console.WriteLine($"You chose {shosenPath} path.\n");
+                            RunFraction();
                             break;
                         }
                     case "3 1":
-                        Console.WriteLine($"You chose {shosenPath} path.\n");
-                        GeniusIdiot.TryExampleOfWork();
-                        PrintBFPL_STEPIK_MainMenu();
-                        break;
+                        {
+                            Console.WriteLine($"You chose {shosenPath} path.\n");
+                            GeniusIdiot.TryExampleOfWork();
+                            PrintBFPL_STEPIK_MainMenu();
+                            break;
+                        }
+                    case "3 2":
+                        {
+                            Console.WriteLine($"You chose {shosenPath} path.\n");
+                            GeniusIdiot.TryExampleOfWork();
+                            PrintBFPL_STEPIK_MainMenu();
+                            break;
+                        }
                     case "menu":
-                        PrintBFPL_STEPIK_MainMenu();
-                        break;
+                        {
+                            PrintBFPL_STEPIK_MainMenu();
+                            break;
+                        }
                     case "clear":
-                        Console.Clear();
-                        PrintBFPL_STEPIK_MainMenu();
-                        break;
+                        {
+                            Console.Clear();
+                            PrintBFPL_STEPIK_MainMenu();
+                            break;
+                        }
                     case "return":
-                        //Go to BFPL main menu
-                        BFPL.BFPL_MainMenu.PrintMainMenu();
-                        return;
+                        {
+                            //Go to BFPL main menu
+                            BFPL.BFPL_MainMenu.PrintMainMenu();
+                            return;
+                        }
                     default:
-                        Console.WriteLine($"Path: [{shosenPath}] do not exist.\n");
-                        break;
+                        {
+                            Console.WriteLine($"Path: [{shosenPath}] do not exist.\n");
+                            break;
+                        }
                 }
             }
+        }
+
+        private static void RunFraction()
+        {
+            Fraction first = new Fraction(3, 9);
+            first.PrintFraction();
+            Fraction second = new Fraction(63, 21);
+            second.PrintFraction();
+            int third = 5;
+
+            (first + second).PrintFraction();
+            (first - second).PrintFraction();
+            (first * second).PrintFraction();
+            (first / second).PrintFraction();
+
+            (first + third).PrintFraction();
+            (first - third).PrintFraction();
+            (first * third).PrintFraction();
+            (first / third).PrintFraction();
+
+            (third + first).PrintFraction();
+            (third - first).PrintFraction();
+            (third * first).PrintFraction();
+            (third / first).PrintFraction();
+
+            //Fraction last = new Fraction(3, 0); // Выбрасывает исключение из-за деления на ноль
+
+            PrintBFPL_STEPIK_MainMenu();
+        }
+
+        // Методы для обработки пунктов меню
+        public static void RunXesAndZeros()
+        {
+            XesAndZeros.GoXesAndZeros();
+            PrintBFPL_STEPIK_MainMenu();
+        }
+
+        public static void RunPRGSW()
+        {
+            string mathExpBeforePRGSW = $"( ( - 2 + 3 ) * ( 4 - 1 ) ) ^ ( 2 + ( 1 + 1 ) )";
+            string mathExpAfterPRGSW = $"2 - 3 + 4 1 - * 2 1 1 + + ^";
+            Console.WriteLine(mathExpBeforePRGSW);
+            Console.WriteLine(PRGSW.polishReverseGripSteeringWheel(mathExpBeforePRGSW));
+            Console.WriteLine(mathExpAfterPRGSW);
+            Console.WriteLine(PRGSW.undoPolishReverseGripSteeringWheel(mathExpAfterPRGSW));
+            Console.WriteLine(mathExpBeforePRGSW);
+            Console.WriteLine(PRGSW.calculatePolishReverseGripSteeringWheel(mathExpBeforePRGSW, false));
+            Console.WriteLine(mathExpAfterPRGSW);
+            Console.WriteLine(PRGSW.calculatePolishReverseGripSteeringWheel(mathExpAfterPRGSW, true));
+            PrintBFPL_STEPIK_MainMenu();
         }
     }
 }
